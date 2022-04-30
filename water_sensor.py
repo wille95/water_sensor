b6a0d4d998154e936512508cbf55313ff7d3e3b8
wille95 <sven.wille@kabelmail.de>
2022-04-30 10:54:27 +0200

    Version 2

--- water_sensor.py
+++ water_sensor.py
@@ -1,10 +1,14 @@
-from machine import Pin, ADC
-from time import sleep
+# Sven Wille ETS2021
+# 30.04.2022
+# IDUINO Water Sensor(SE045)
 
-pot = ADC(Pin(33))
-pot.atten(ADC.ATTN_11DB)       #Full range: 3.3v
+from machine import Pin, ADC        # Pin und ADC (analog Pin) importieren
+from time import sleep              # sleep importieren
+
+analog_Pin = ADC(Pin(33))           # Analog Pin 33 
+analog_Pin.atten(ADC.ATTN_11DB)     # 3.3V Spannungsbereich 
 
 while True:
-  pot_value = pot.read()
-  print(pot_value)
-  sleep(0.1)
+  water_value = analog_Pin.read()   # Analogwert auslesen
+  print(water_value)                # Analogwer ausgeben
+  sleep(0.5)
